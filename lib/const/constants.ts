fb7d8abed8608a2d83c5972b7b7521cb48fa9d9e
DonkeyCo <duc.vongoc01@gmail.com>
2020-08-12 20:21:52 +0200

    Add docstring to constants

--- lib/const/constants.ts
+++ lib/const/constants.ts
@@ -1,6 +1,10 @@
+/** Constants for Epic Games crawler */
 enum Constant {
+    /** Epic Games GraphQL base URL */
     GQL_BASE = "https://graphql.epicgames.com/graphql",
+    /** Epic Games store content base URL */
     SC_BASE = "https://store-content.ak.epicgames.com/api",
+    /** Epic Games store content blog path */
     SC_BLOG = "content/blog"
 };
 
