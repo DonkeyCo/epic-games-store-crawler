c46b9474938ce763bca51e4da41ba5b3e9cef3c5
DonkeyCo <duc.vongoc01@gmail.com>
2022-01-30 15:38:56 +0100

    Update GraphQL base URL

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,7 +1,7 @@
 /** Constants for Epic Games crawler */
 enum Constant {
     /** Epic Games GraphQL base URL */
-    GQL_BASE = "https://graphql.epicgames.com/graphql",
+    GQL_BASE = "https://www.epicgames.com/graphql",
     /** Epic Games store content base URL */
     SC_BASE = "https://store-content.ak.epicgames.com/api",
     /** Epic Games store content blog path */
